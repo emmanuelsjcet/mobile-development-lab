3a4813969a7f6d8350d2795c6175c2ba2da6c3e7
emmzsjcet <125429034+emmanuelsjcet@users.noreply.github.com>
2023-10-10 10:06:07 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,26 +1,72 @@
-package com.example.sjcet.login;
+package com.example.sjcet.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
     }
-    public void click(View view){
-        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
-        {
-            Toast.makeText(this, "valid", Toast.LENGTH_SHORT).show();
-        }
 
+    public void sum(View view) {
+
+        EditText t1=(EditText)findViewById(R.id.editText);
+        EditText t2=(EditText)findViewById(R.id.editText2);
+
+        int x=Integer.parseInt(t1.getText().toString());
+        int y=Integer.parseInt(t2.getText().toString());
+
+        int z=x+y;
+
+        TextView tv1=(TextView)findViewById(R.id.tv);
+        tv1.setText("sum is "+z);
+    }
+
+    public void mult(View view) {
+
+        EditText t1=(EditText)findViewById(R.id.editText);
+        EditText t2=(EditText)findViewById(R.id.editText2);
+
+        int x=Integer.parseInt(t1.getText().toString());
+        int y=Integer.parseInt(t2.getText().toString());
+
+        int m=x*y;
+
+        TextView tv1=(TextView)findViewById(R.id.tv);
+        tv1.setText("sum is "+m);
+    }
+
+    public void div(View view) {
+
+        EditText t1=(EditText)findViewById(R.id.editText);
+        EditText t2=(EditText)findViewById(R.id.editText2);
+
+        int x=Integer.parseInt(t1.getText().toString());
+        int y=Integer.parseInt(t2.getText().toString());
+
+        int d=x/y;
+
+        TextView tv1=(TextView)findViewById(R.id.tv);
+        tv1.setText("sum is "+d);
+
+    }
+
+    public void sub(View view) {
+        EditText t1=(EditText)findViewById(R.id.editText);
+        EditText t2=(EditText)findViewById(R.id.editText2);
+
+        int x=Integer.parseInt(t1.getText().toString());
+        int y=Integer.parseInt(t2.getText().toString());
+
+        int s=x/y;
+
+        TextView tv1=(TextView)findViewById(R.id.tv);
+        tv1.setText("sum is "+s);
     }
 }
